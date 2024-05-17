ed1bd812ee0f68981db00edc19622bc584e2b7ce
haseeb2412 <haseeb241202@gmail.com>
2024-05-18 01:30:54 +0500

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Banner from './Components/Banner'
+
+function App() {
+  return (
+    <>
+
+  <Navbar/>
+  <Banner/>
+  </>
+
+  )
+}
+
+export default App
