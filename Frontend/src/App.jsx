b08561bb018d1e7bc1792f6f9a013153ebf4ef9e
haseeb2412 <haseeb241202@gmail.com>
2024-05-18 10:27:55 +0500

    working on navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Banner from './Components/Banner'
+import Footer from './Components/Footer'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
 
   <Navbar/>
   <Banner/>
+  <Footer/>
   </>
 
   )
--- Frontend/src/Components/Navbar.jsx
+++ Frontend/src/Components/Navbar.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Navbar() {
+  const [sticky,setSticky]=useState(false);
+  useEffect(()=>{
+    const handleScroll = ()=>{
+      if(window.scroll>0){
+        setSticky(true);
+      }else{
+        setSticky(false);
+      }
+    }
+    window.addEventListener('scroll',handleScroll)
+    return()=>{
+      window.removeEventListener('scroll',handleScroll)
+    }
+  },[])
   const navItem = (
     <>
       <li>
@@ -19,8 +33,10 @@ function Navbar() {
     </>
   );
   return (
-    <div className="max-w-screen-2xl container mx-auto md:px-20 px-4 ">
-        <div className="navbar bg-base-100">
+    <div className={`max-w-screen-2xl container mx-auto md:px-20 px-4 fixed top-0 left-0 right-0 ${
+      sticky ? "sticky-navbar shadow-md bg-base-200 duration-300 transition-all ease-in-out":""
+    }`}>
+        <div className="navbar ">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
